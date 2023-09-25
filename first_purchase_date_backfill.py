e29a13a9c0aa2a2dec79b57fe83e93f601543241
Pav <pavan_1994@hotmail.co.uk>
2023-09-25 13:57:32 +0100

    Create first_purchase_date_backfill.py

---
+++ first_purchase_date_backfill.py
@@ -0,0 +1,156 @@
+import requests
+from datetime import datetime
+from time import time, sleep
+import threading
+import logging
+
+# ​ Script Execution ###​
+
+# Account Information
+SEGMENT_ID = ""
+METRIC_ID = ""
+PRIVATE_API_KEY = ""
+logging.basicConfig(filename='script.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
+
+current_cursor = 'STARTED'
+profile_count = 0
+# Helper methods
+
+
+def set_headers():
+    headers = {
+        "accept": "application/json",
+        "revision": "2023-09-15",
+        "Authorization": f"Klaviyo-API-Key {PRIVATE_API_KEY}",
+    }
+    return headers
+
+
+def get_segment_profiles(cursor=""):
+    print('get profiles', time())
+    global current_cursor
+    headers = set_headers()
+    # Handling for non 200 response
+    try:
+        response = requests.get(cursor, headers=headers)
+        response.raise_for_status()  # Raises an exception for non-200 responses
+        data = response.json().get('data')
+        links = response.json().get('links').get('next')
+
+        profile_ids = []
+        for item in data:
+            profile_ids.append(item["id"])
+
+        if links:
+            current_cursor = links
+            print('current cursor', current_cursor)
+        else:
+            print('FINISHED PAGINATION!')
+            current_cursor = 'DONE'
+
+        return profile_ids
+    except requests.exceptions.HTTPError as e:
+        print(f"Error getting segment profiles: {e}")
+        return []
+
+    return profile_ids
+
+
+def get_properties_of_first_event_for_profile(profile_id, metric_id):
+    # print('get events', time.time())
+    url = f"https://a.klaviyo.com/api/events/?filter=equals(profile_id,'{profile_id}'),and(equals(metric_id,'{metric_id}'))&sort=datetime&page[size]=1"
+    headers = set_headers()
+    response = ''
+    data = ''
+    # Handling non 200 response & logging
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json().get('data')
+    except requests.exceptions.HTTPError as e:
+        print(
+            f"Error fetching event for {profile_id}: HTTP {e.response.status_code}")
+        logging.error("Error fetching event for %s: HTTP %s",
+                      profile_id, e.response.status_code)
+
+    except:
+        print("Error fetching event for ", profile_id,  response)
+        logging.error('Error fetching event for %s %s', profile_id, response)
+
+    if len(data) > 0:
+        # DATA MAY BE NESTED DIFFERENTLY FOR YOUR PLACED ORDER EVENT - MAKE CHANGES AS REQUIRED
+        # Use POSTMAN to Evaluate with a GET request using the URL above
+        # print(event_properties)
+        event_properties = data[0]["attributes"]["event_properties"]
+        event_items = event_properties.get("Items")
+        event_value = event_properties.get("$value")
+        first_purchase_date = event_properties.get(
+            '$extra').get('created_at')[:10]
+        return {
+            "First Purchase Date": first_purchase_date,
+            "First Purchase Value": event_value,
+            "First Purchase Products": event_items
+        }
+    else:
+        print('data missing for ', profile_id, data)
+        logging.error('Data missing for %s', profile_id)
+    return {}
+
+
+def set_properties_for_profile(profile_id, properties):
+    url = f"https://a.klaviyo.com/api/profiles/{profile_id}"
+    headers = set_headers()
+    payload = {"data": {
+        "type": "profile",
+        "id": f"{profile_id}",
+        "attributes": {
+                "properties": properties
+        }
+    }
+    }
+    try:
+        response = requests.patch(url, json=payload, headers=headers)
+        if(response.status_code != 200):
+            logging.error('Update error %s ', response.status_code)
+            logging.error('Reponse is %s', response.json())
+    except:
+        logging.error('something went wrong with %s %s',
+                      profile_id, properties)
+
+    global profile_count
+    profile_count = profile_count + 1
+    print('Updated ', profile_count, ' profiles so far', time())
+
+
+def threaded_update(profile_id, metric_id):
+    new_properties = get_properties_of_first_event_for_profile(
+        profile_id, METRIC_ID)
+    if new_properties and isinstance(new_properties, dict) and new_properties:
+        # Check if new_properties is not None, is a dictionary, and not empty
+        set_properties_for_profile(profile_id, new_properties)
+    else:
+        print(
+            f"Skipping profile {profile_id} due to invalid or empty properties")
+
+# Main script execution
+
+
+def main():
+    global current_cursor
+    current_cursor = f"https://a.klaviyo.com/api/segments/{SEGMENT_ID}/profiles/?page[size]=100"
+
+    while current_cursor != "DONE":
+        # Get IDs of all profiles belonging into the sepcified segment
+        profile_ids = get_segment_profiles(current_cursor)
+        # print(profile_ids)
+        # Extract desired properties from their first Placed Order event
+        for profile_id in profile_ids:
+            # we have a limit of 700 updates per min, so need to sleep briefly
+            sleep(60/700)
+            threading.Thread(target=threaded_update, args=[
+                             profile_id, METRIC_ID]).start()
+
+
+if __name__ == "__main__":
+    main()
